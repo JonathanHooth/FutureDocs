7b0e746e47995ca8ffc3dfcd385eff27577b7441
zaybournand <zaybournand@gmail.com>
2025-01-25 23:50:17 -0500

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,41 +1,31 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
-import "./NavBar.css"
+function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Example state for user login status
 
-function NavBar(){
-
-
-
-  return(
+  return (
     <>
-    <div className="navBarContainer">
-      <Link className="navLink" to="/" >
-      <div className="title">
-        SwampHacks
-      </div>
-      </Link>
+      <div className="navBarContainer">
+        <Link className="navLink" to="/">
+          <div className="title">SwampHacks</div>
+        </Link>
 
-      <div className="rightSided">
-        <div className="account">
-          <button className="button">
-            Sign In
-          </button>
-        </div>
-        <div className="time">
-          11:06 AM
-        </div>
-        <div className="date">
-          26 Jan 2025
+        <div className="rightSided">
+          <div className="account">
+            <Link className="navLink" to="/auth">
+              <button className="button">
+                {isLoggedIn ? "Log Out" : "Sign In"}
+              </button>
+            </Link>
+          </div>
+          <div className="time">11:06 AM</div>
+          <div className="date">26 Jan 2025</div>
         </div>
       </div>
-
-    </div>
-    
     </>
-  )
-
+  );
 }
 
-
-export default NavBar
+export default NavBar;
